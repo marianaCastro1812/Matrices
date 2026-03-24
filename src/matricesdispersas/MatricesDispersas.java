22da5a199e9f9d14a5488abd13b2728cd516b387
Maria <Maria@Asus-Mari>
2026-03-23 20:08:38 -0500

    commit one

---
+++ src/matricesdispersas/MatricesDispersas.java
@@ -0,0 +1,143 @@
+package matricesdispersas;
+
+import javax.swing.JOptionPane;
+import java.util.Random;
+/**
+ *
+ * @author Mariana Castro, Miguel Gallo y Gepetto
+ */
+public class MatricesDispersas {
+
+    public static void main(String[] args) {
+      
+     //int Mat[][]= { {8,0,5,0,3,0},{0,-11,0,0,-2,0},{1,0,0,6,0,19}}; //3x6
+     
+   
+     
+     
+     int Mat2 [][]= { {1,3}, {4,6},{1,1},{7,3},{2,2},{5,6}}; //6x2
+     int N2=contarDatos(Mat2);
+     Tripleta T2= new Tripleta(N2);
+     T2.llenarTripleta(Mat2,N2);
+     //T2.multiplicarTripletas(T1);
+    
+    //MENU
+    Tripleta T1 =null; 
+     int opc = 0;
+     
+
+
+        do {
+            
+            opc = Integer.parseInt(JOptionPane.showInputDialog(" Menu Principal\n"
+                + "1. Crear Tripleta\n"
+                + "2. Mostrar Forma\n"
+                + "3. Suma de filas\n"
+                + "4. Suma de columnas\n"
+                + "5. Insertar Dato \n"
+                + "6. Eliminar Dato\n"
+                + "7. Eliminar Dato por Posicion\n"
+                + "8. Sumar\n"
+                + "9. Multiplicar\n"
+                + "0. Salir\n"
+     
+     
+                + "Ingrese que Forma necesita"));
+            
+            switch (opc) {
+                case 1:
+                    
+                    Random r = new Random();
+
+
+                    int f = r.nextInt(5) + 2;      
+                    int c = r.nextInt(5) + 2;   
+
+                    int[][] Mat = new int[f][c];
+                    for (int i = 0; i < Mat.length; i++) {
+                        for (int j = 0; j < Mat[i].length; j++) {
+
+                            int prob = r.nextInt(100);
+
+                            if (prob < 70) {
+                                Mat[i][j] = 0;
+                            } else {
+                                Mat[i][j] = r.nextInt(20) + 1;
+                            }
+                        }
+                    }
+                    int N = contarDatos(Mat);
+
+                    T1 = new Tripleta(N);
+                    T1.llenarTripleta(Mat, N);
+                    
+             
+                    
+                    break;
+
+                case 2:
+                    if (T1 != null){
+            T1.mostrar();
+        } else {
+            JOptionPane.showMessageDialog(null, "Primero debes crear la Matriz");
+        }
+        break;
+                    
+                    
+                 
+
+                case 3:
+                   
+                    break;
+
+                case 4:
+                 
+                    break;
+
+                case 5:
+                    boolean valido = false;
+                    if (T1 != null){
+                        while(!valido){
+                    try {
+                        int fila = Integer.parseInt(JOptionPane.showInputDialog("Ingresa una fila"));
+                        int columna = Integer.parseInt(JOptionPane.showInputDialog("Ingresa una columna"));
+                        int dato = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un dato"));
+                        T1.insertar(fila, columna, dato);
+                        valido=true;
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Debes ingresar números válidos");
+                    }
+                        }
+            
+        } else {
+            JOptionPane.showMessageDialog(null, "Primero debes crear la Matriz");
+        }
+                    
+            
+                    break;
+                    
+                case 0:
+
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Esta opcion es invalida");
+            }
+        } while (opc!=0);
+    }
+    
+    public static int contarDatos(int Mat[][]){
+        
+        int N=0;
+        
+        for (int i = 0; i < Mat.length; i++) {
+            for (int j = 0; j < Mat[0].length; j++) {
+                if (Mat[i][j]!=0){
+                    N++;
+                }
+            }
+        }
+        
+        return N;
+    }
+}
---
+++ src/matricesdispersas/Tripleta.java
@@ -0,0 +1,119 @@
+//
+package matricesdispersas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Maria
+ */
+public class Tripleta {
+    //Atributos
+    private int Mtri[][];
+    
+    //Metodos
+
+    public Tripleta(int N) {
+        
+        Mtri = new int [N+1][3];
+        
+    }
+
+    public int getMtri(int k, int i) {
+        return Mtri[k][i];
+    }
+
+    public void setMtri(int k, int i, int d) {
+        Mtri[k][i] = d;
+    }
+    
+    public void llenarTripleta(int Mat[][],int N){
+        int k=1;
+        
+        Mtri[0][0]=Mat.length;
+        Mtri[0][1]=Mat[0].length;
+        Mtri[0][2]=N;
+        
+        for (int i = 0; i < Mat.length; i++) {
+            for (int j = 0; j < Mat[0].length; j++) {
+                if (Mat[i][j]!=0){
+                    Mtri[k][0]=i;
+                    Mtri[k][1]=j;
+                    Mtri[k][2]=Mat[i][j];
+                    k++;
+                }
+            }
+        }
+    }
+    
+    public void insertar(int f, int c, int d){
+        
+        if (( f >= 0 && f < Mtri[0][0])&& (c>=0 && c < Mtri[0][1])){
+        
+        boolean x=false;
+        for (int k = 1; k < Mtri.length; k++) {
+            
+               if (Mtri[k][0]==f && Mtri[k][1]==c) {
+                   Mtri[k][2]+=d;
+                   x=true;
+                   break;
+                
+            }
+            
+            
+        }
+            if (x==false) {
+                int[][] aux= new int[Mtri.length+1][Mtri[0].length];
+                aux[0][0]=Mtri[0][0];
+                aux[0][1]=Mtri[0][1];
+                aux[0][2]=Mtri[0][2]+1;
+                 int k=1;  
+                 int i=1;
+                 boolean insertado=false;
+                 while(i < Mtri.length){
+                    if (!insertado && (f < Mtri[i][0] || f ==Mtri[i][0] && c< Mtri[i][1])){
+                        aux[k][0]=f;
+                        aux[k][1]=c;
+                        aux[k][2]=d;
+                        k++;
+                        insertado = true;
+                        
+                    }
+                        aux[k][0]=Mtri[i][0];
+                        aux[k][1]=Mtri[i][1];
+                        aux[k][2]=Mtri[i][2];
+                        k++;
+                        i++;
+                        
+                    
+                }
+                 if (!insertado){
+                        aux[k][0]=f;
+                        aux[k][1]=c;
+                        aux[k][2]=d;
+                 }
+                 Mtri=aux;
+                 }
+        
+            
+      } else{
+            JOptionPane.showInputDialog(" La fila "+f + " y la columna" + c + "no se pueden agregar a la Tripleta");
+        }
+    }
+        
+    public void mostrar(){
+        String p="";
+        
+        for (int k=0; k <= Mtri[0][2];k++){
+            p+="|"+ Mtri[k][0]+"|"+Mtri[k][1]+"|"+Mtri[k][2]+"| \n";
+        }
+        JOptionPane.showMessageDialog(null, p);
+    }
+    
+    
+ public void multiplicarTripletas(Tripleta T1){
+     
+     
+ }   
+    
+}
